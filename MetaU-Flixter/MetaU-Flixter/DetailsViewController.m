a78cb64d9c55d937e6ce63a0fb9b46a3ad50343b
Maribel Montejano <outdoorsole@users.noreply.github.com>
2022-06-21 04:59:04 -0700

    Set title and synopsis labels for DetailsViewController

--- MetaU-Flixter/MetaU-Flixter/DetailsViewController.m
+++ MetaU-Flixter/MetaU-Flixter/DetailsViewController.m
@@ -9,25 +9,20 @@
 
 @interface DetailsViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *backdropImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *posterImageView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+
 @end
 
 @implementation DetailsViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    NSLog(@"Here is the movie:");
-    NSLog(@"%@", self.movie);
+    
+    self.titleLabel.text = self.movie[@"title"];
+    self.synopsisLabel.text = self.movie[@"overview"];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
