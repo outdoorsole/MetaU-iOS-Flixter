38318d5cac83b4fd62186796da69fe753c9cee7c
Maribel Montejano <outdoorsole@users.noreply.github.com>
2022-06-21 04:19:37 -0700

    Add DetailsViewController, segue from MovieCell

---
+++ MetaU-Flixter/MetaU-Flixter/DetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailsViewController.h
+//  MetaU-Flixter
+//
+//  Created by m on 6/21/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
