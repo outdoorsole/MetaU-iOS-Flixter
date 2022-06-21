6414376ba3b978d4f38e2a5bd856d458b1ea6312
Maribel Montejano <outdoorsole@users.noreply.github.com>
2022-06-21 04:10:27 -0700

    Create and connect outlets for MovieCell

---
+++ MetaU-Flixter/MetaU-Flixter/MovieCell.h
@@ -0,0 +1,20 @@
+//
+//  MovieCell.h
+//  MetaU-Flixter
+//
+//  Created by m on 6/21/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIView *posterImageView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MetaU-Flixter/MetaU-Flixter/MovieCell.m
@@ -0,0 +1,23 @@
+//
+//  MovieCell.m
+//  MetaU-Flixter
+//
+//  Created by m on 6/21/22.
+//
+
+#import "MovieCell.h"
+
+@implementation MovieCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
