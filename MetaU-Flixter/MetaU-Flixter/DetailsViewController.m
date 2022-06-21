1b5b1e0916d58058ca8a4d7fd2d50f9c1acb1cce
Maribel Montejano <outdoorsole@users.noreply.github.com>
2022-06-21 05:30:54 -0700

    Set backdrop and poster image views in DetailsViewController

--- MetaU-Flixter/MetaU-Flixter/DetailsViewController.m
+++ MetaU-Flixter/MetaU-Flixter/DetailsViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "DetailsViewController.h"
+#import <UIImageView+AFNetworking.h>
 
 @interface DetailsViewController ()
 
@@ -23,6 +24,19 @@
     
     self.titleLabel.text = self.movie[@"title"];
     self.synopsisLabel.text = self.movie[@"overview"];
+
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/";
+    NSString *fullBackdropURLString = [baseURLString stringByAppendingString:@"w500"];
+    fullBackdropURLString = [fullBackdropURLString stringByAppendingString:self.movie[@"backdrop_path"]];
+    NSURL *backdropURL = [NSURL URLWithString:fullBackdropURLString];
+
+    [self.backdropImageView setImageWithURL:backdropURL];
+
+    NSString *fullPosterURLString = [baseURLString stringByAppendingString:@"w500"];
+    fullPosterURLString = [fullPosterURLString stringByAppendingString:self.movie[@"poster_path"]];
+    NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
+
+    [self.posterImageView setImageWithURL:posterURL];
 }
 
 @end
